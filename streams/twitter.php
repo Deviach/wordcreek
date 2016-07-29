8c196c3e5712ac7edb2cda8e83ab2a10c9236ab9
deviach <therixy@gmail.com>
2016-07-29 19:12:22 -0400

    added twitter library

--- streams/twitter.php
+++ streams/twitter.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../twitter-api-php-master/TwitterAPIExchange.php';
+require_once '../lib/twitter-api-php-master/TwitterAPIExchange.php';
 require_once '../config.php';
 $settings = array(
   'consumer_key'              => $TWITTER_CONSUMER_KEY,
@@ -8,8 +8,12 @@ $settings = array(
   'oauth_access_token_secret'       => $TWITTER_OAUTH_SECRET,
 );
 $hashtag = "news";
+$format = true;
 if (isset($_GET['hashtag'])){
 	$hashtag = $_GET['hashtag'];
+}
+if (isset($_GET['format'])){
+	$format = $_GET['format'];
 }
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
 $requestMethod = 'GET';
@@ -20,8 +24,9 @@ $data = $twitter->setGetField($getfield)
 	     ->buildOauth($url,$requestMethod)
 		->performRequest();
 $data = json_decode($data);
-//var_dump($data);
-//var_dump($data->statuses[0]->text);
+
+
+
 $maxc = 10;
 for ($i = 0; $i<count( $data->statuses );$i++){
 	echo "<div><br> >>>>>>>>>>".$data->statuses[$i]->created_at."</div>";
