497de3d78936bee88bb61a9fc2e06769790a0412
deviach <therixy@gmail.com>
2016-07-29 20:19:22 -0400

    update random

--- streams/random.php
+++ streams/random.php
@@ -1,12 +1,21 @@
 <?php
 $max = 5;
-$rnd = random_int(1,$max);
+$url = "http://endorphingames.altervista.org/wordcreek/streams/";
+$rnd = rand(1,$max);
 if ($rnd == 1){
-	$txt = file_get_contents('http://endorphingames.altervista.org/constream/weather.php';
+	$txt = file_get_contents($url."weather.php");
+	echo $txt;
+}
+else if ($rnd == 2){
+	$txt = file_get_contents($url."youtube.php");
+	echo $txt;
+}
+else if ($rnd == 3){
+	$txt = file_get_contents($url."twitter.php?hashtag=usnews");
 	echo $txt;
 }
 else{
-	$txt = file_get_contents('http://example.com');
+	$txt = file_get_contents('http://pastebin.com/raw/RHwVWzSr');
 	echo $txt;
 }
 ?>
